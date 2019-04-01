db999b28d1b335ebaeab1b7a22c07f464404e42a
Hiroki <ll@lls-macbook-pro.local>
2019-04-01 00:44:28 -0400

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Hiroki on 2019/03/31.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
